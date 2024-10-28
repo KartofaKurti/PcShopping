a8ae0b8477071cbe7d36da09716c43338adbe9a4
KartofaKurti <krafter420@gmail.com>
2024-10-29 00:47:45 +0200

    Test

---
+++ PcBuilder.Common/ProductCons.cs
@@ -0,0 +1,19 @@
+﻿namespace PcBuilder.Common
+{
+    public class ProductCons
+    {
+        public const int MaxlengthProductName = 100;
+        public const int MinlengthProductName = 5;
+        public const int MinlengthDescription = 5;
+        public const int MaxlengthImageUrl = 2048;
+    }
+
+    public class UserValidation
+    {
+        public const int MinLengthFirstName = 3;
+        public const int MaxlengthFirstName = 20;
+        public const int MinLengthSecondName = 3;
+        public const int MaxlengthSecondName = 30;
+    }
+
+}
---
+++ PcBuilder.Data.Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcBuilder.Data.Models
+{
+    public class Category
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(30)]
+        public string CategoryName { get; set; }
+    }
+}
